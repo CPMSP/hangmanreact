865d03b124ed3d339c80069879e6aa19fa49fa4a
CPMSP <cpetersonmsp@gmail.com>
2021-01-11 23:34:38 -0600

    redeploy

---
+++ build/precache-manifest.807ff3abcb156774caa8094d8aa6f6fa.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f3953682901f5db4635aff5da5b358c",
+    "url": "/hangmanreact/index.html"
+  },
+  {
+    "revision": "0bf81010bacbd50bd8e3",
+    "url": "/hangmanreact/static/css/main.f1a276ef.chunk.css"
+  },
+  {
+    "revision": "072aa0d68f99c82792a1",
+    "url": "/hangmanreact/static/js/2.cd6a6757.chunk.js"
+  },
+  {
+    "revision": "78b86c0095c74f94992db9795d80e258",
+    "url": "/hangmanreact/static/js/2.cd6a6757.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0bf81010bacbd50bd8e3",
+    "url": "/hangmanreact/static/js/main.6c731d1e.chunk.js"
+  },
+  {
+    "revision": "16119c9e1c26ac6a30ef",
+    "url": "/hangmanreact/static/js/runtime-main.9291204e.js"
+  },
+  {
+    "revision": "db9097e874e68b7ed7115ff05ffcb880",
+    "url": "/hangmanreact/static/media/0.db9097e8.jpg"
+  },
+  {
+    "revision": "fbff4784004cc97463c883477a0090d1",
+    "url": "/hangmanreact/static/media/1.fbff4784.jpg"
+  },
+  {
+    "revision": "c4ea9528b67823bd94c90de3d97d75a3",
+    "url": "/hangmanreact/static/media/2.c4ea9528.jpg"
+  },
+  {
+    "revision": "62a781c89c590abb27129638d9d18c4d",
+    "url": "/hangmanreact/static/media/3.62a781c8.jpg"
+  },
+  {
+    "revision": "0f8ca81ee9f7b2bc27102267f9e23864",
+    "url": "/hangmanreact/static/media/4.0f8ca81e.jpg"
+  },
+  {
+    "revision": "1b557aa7ccfa88b6b3f4723c27d3f2d6",
+    "url": "/hangmanreact/static/media/5.1b557aa7.jpg"
+  },
+  {
+    "revision": "c80394c2a96ffad09a9df4e4815745f2",
+    "url": "/hangmanreact/static/media/6.c80394c2.jpg"
+  }
+]);
